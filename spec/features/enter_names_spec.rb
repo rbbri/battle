30daff3b1785f38c15414a7a8878d56fda0cd515
rbbri <robin.s.johnson@gmail.com>
2018-10-16 14:21:54 +0100

    added web helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -3,15 +3,11 @@
 # We want to Start a fight by entering our names and seeing them
 feature 'Entering player names' do
   scenario 'P1 enters name' do
-    visit('/')
-    fill_in('p1', with: 'John')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'John'
   end
   scenario 'P2 enters name' do
-    visit('/')
-    fill_in('p2', with: 'Not John')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Not John'
   end
 end
