792790eb61c113eac12fa41ddaec254c2f53d60a
rbbri <robin.s.johnson@gmail.com>
2018-10-16 13:56:33 +0100

    added html styling

--- app.rb
+++ app.rb
@@ -1,16 +1,13 @@
 require 'sinatra/base'
 
-
 class Battle < Sinatra::Base
-
-  get "/" do
+  get '/' do
     erb(:index)
   end
 
-  post "/names" do
+  post '/names' do
     @p1 = params[:p1]
     @p2 = params[:p2]
     erb(:play)
   end
-
 end
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,18 +1,17 @@
 # As two Players,
 # So we can play a personalised game of Battle,
 # We want to Start a fight by entering our names and seeing them
-feature "Entering player names" do
-  scenario "P1 enters name" do
+feature 'Entering player names' do
+  scenario 'P1 enters name' do
     visit('/')
-    fill_in('p1', with: "John")
-    click_button("Submit")
-    expect(page).to have_content "John"
+    fill_in('p1', with: 'John')
+    click_button('Submit')
+    expect(page).to have_content 'John'
   end
-  scenario "P2 enters name" do
+  scenario 'P2 enters name' do
     visit('/')
-    fill_in('p2', with: "Not John")
-    click_button("Submit")
-    expect(page).to have_content "Not John"
+    fill_in('p2', with: 'Not John')
+    click_button('Submit')
+    expect(page).to have_content 'Not John'
   end
-
 end
