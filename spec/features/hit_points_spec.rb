18a0f6b0b5cf6fc4bd1543144039e35c54d34ed2
rbbri <robin.s.johnson@gmail.com>
2018-10-17 14:23:46 +0100

    changed hit_points feature spec in preparation for random attacks

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -7,12 +7,12 @@ feature 'Hit Points' do
   scenario 'player 1 attacks player 2' do
     sign_in_and_play
     click_button 'Attack'
-    expect(page).to have_content 'Not John: 50HP'
+    expect(page).to have_no_content 'Not John: 60HP'
   end
   scenario 'player 1 attacks player 2' do
     sign_in_and_play
     click_button 'Attack'
     click_button 'Attack'
-    expect(page).to have_content 'John: 50HP'
+    expect(page).to have_no_content 'John: 60HP'
   end
 end
